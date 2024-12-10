7829a34a439fe5ee5cb39a9d78f9306c3726da22
sl1m41kc <ricogyrchik@gmail.com>
2024-12-10 10:38:03 +0300

    add modal window

---
+++ app/data/modal_data.tsx
@@ -0,0 +1,6 @@
+export const modal_data = [
+    {
+        title: <>Заголовок модального окна</>,
+        text: <>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore, quam ullam blanditiis mollitia minima facilis iusto necessitatibus illo aliquam culpa sed quis at delectus in error hic dicta, inventore voluptatem.</>,
+    },
+]
