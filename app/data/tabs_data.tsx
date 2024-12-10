0a51bfb1bdf6492947efa0a04759249ace43a6b0
sl1m41kc <ricogyrchik@gmail.com>
2024-12-10 23:54:59 +0300

    add tabs

---
+++ app/data/tabs_data.tsx
@@ -0,0 +1,42 @@
+const tabs_data = [
+    {
+        title: <>Таб 1</>,
+        content: {
+            title: <>Заголовок таба 1</>,
+            text: <>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut provident ipsam ut sed voluptatem quasi consectetur quibusdam atque, laudantium et tempore voluptatum deserunt, maiores impedit reprehenderit eum modi veniam dicta!</>,
+        }
+    },
+    {
+        title: <>Таб 2</>,
+        content: {
+            title: <>Заголовок таба 2</>,
+            text: <>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex non voluptates, vitae repellat quis aliquid voluptas corporis exercitationem, accusantium libero quasi dicta, nulla eos corrupti quas dolorum cumque molestias quaerat.
+            Facilis aliquid voluptates fugit nulla iste minima, nihil, laudantium non esse optio minus reiciendis aspernatur earum amet. Incidunt sequi, iure sapiente expedita est praesentium molestias autem harum impedit accusantium velit?</>,
+        }
+    },
+    {
+        title: <>Таб 3</>,
+        content: {
+            title: <>Заголовок таба 3</>,
+            text: <>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab eos nihil soluta non error aperiam iusto possimus distinctio ipsam veniam. Labore asperiores pariatur animi hic? Reprehenderit incidunt nam iste exercitationem.</>,
+        }
+    },
+    {
+        title: <>Таб 4</>,
+        content: {
+            title: <>Заголовок таба 4</>,
+            text: <>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde odit, explicabo quis delectus distinctio doloribus illum excepturi reiciendis. Laudantium deserunt vero, animi eligendi fuga officiis architecto consectetur voluptatibus eaque ipsam.
+            Placeat, similique nam vero illo repudiandae accusamus quas vitae doloremque. Inventore ab a exercitationem quas architecto dolore veniam dolorem nisi vel quisquam eaque officiis, perspiciatis voluptatibus saepe, est debitis corrupti?
+            Facere laborum, distinctio aut autem officiis explicabo consequatur quod incidunt praesentium aperiam ducimus animi excepturi! Officia magnam neque at. Magnam dolores fuga unde quasi voluptatum maxime incidunt quam inventore distinctio.</>,
+        }
+    },
+    {
+        title: <>Таб 5</>,
+        content: {
+            title: <>Заголовок таба 5</>,
+            text: <>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut provident ipsam ut sed voluptatem quasi consectetur quibusdam atque, laudantium et tempore voluptatum deserunt!</>,
+        }
+    },
+];
+
+export default tabs_data;
